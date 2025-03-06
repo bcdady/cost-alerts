6c420026436264e6f109eae18dd8caeb3d2c62fb
Bryan Dady <bryan@dady.us>
2025-03-06 06:31:00 -0700

    ops: add output for azure module
    Signed-off-by: Bryan Dady <bryan@dady.us>

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,1 +1,4 @@
 
+output "azurerm_cost_anomaly_alert_name" {
+  value = module.cost-anomaly-detector.anomaly_monitor_name
+}
--- modules/azure-cost-monitoring/outputs.tf
+++ modules/azure-cost-monitoring/outputs.tf
@@ -1,1 +1,9 @@
+output "anomaly_monitor_id" {
+  description = "The ID of the Azure Cost Management anomaly monitor"
+  value       = azurerm_cost_anomaly_alert.this.id
+}
 
+output "anomaly_monitor_name" {
+  description = "The Name of the Azure Cost Management anomaly monitor"
+  value       = "azure-cost-anomaly-${local.subscription_display_name}"
+}
