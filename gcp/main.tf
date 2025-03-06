1b686e29c27eadc6385ac1c650e2471c243d3f9d
Bryan Dady <bryan@dady.us>
2025-03-06 08:58:15 -0700

    chore(gcp): move provider block
    Signed-off-by: Bryan Dady <bryan@dady.us>

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  # credentials = file(var.gcp_credentials_file)
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
 module "cost-anomaly-detector" {
   source = "../modules/gcp-cost-monitoring"
 
